29d6ffba6587ab1799966c07720762d27de3065f
Josh Francisco <josh@franciscofs.com>
2022-05-09 15:44:49 -0700

    Home Screen, etc fixes. -Josh

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import NotFound from './pages/NotFound';
+import { useState } from 'react';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='page'>
+      <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </Router>
     </div>
   );
 }
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+      <div className='p-10'>
+          <div className='bg-[#00000016] w-screen h-screen rounded-sm flex flex-col'>
+                <h1 className='p-10 text-5xl'>
+                The page you requested does not exist!
+                </h1>
+            </div>
+      </div>
+  )
+}
+
+export default NotFound
