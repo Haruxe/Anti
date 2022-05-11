7f229f63a9fa77a666193da9508ee3a14da226b5
Bryce Palichuk <43261252+Brycepalichuk@users.noreply.github.com>
2022-05-11 15:47:04 -0600

    Clean up Navbar

--- .history/src/components/Navbar_20220511153749.tsx
+++ .history/src/components/Navbar_20220511153749.tsx
@@ -1,11 +1,38 @@
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import { Triangle } from 'styled-icons/bootstrap';
+import { Layout, Tabs } from "antd";
 import "antd/dist/antd.css";
+// import "./style.css"
 import Account from "./Account/Account";
 import Chains from "./Chains/Chains";
 import NativeBalance from './NativeBalance';
+
 import '../index.css';
+const { Header } = Layout;
+
+const styles = {
+  header: {
+    // position: "fixed",
+    zIndex: 1,
+    width: "100%",
+    background: "#fff",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    fontFamily: "Roboto, sans-serif",
+    borderBottom: "2px solid rgba(0, 0, 0, 0.06)",
+    padding: "0 10px",
+    boxShadow: "0 1px 10px rgb(151 164 175 / 10%)",
+  },
+  headerRight: {
+    display: "flex",
+    gap: "20px",
+    alignItems: "center",
+    fontSize: "15px",
+    fontWeight: "600",
+  },
+};
 
 function Navbar() {
   return (
