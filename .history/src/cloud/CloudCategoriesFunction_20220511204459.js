c4976d79e24c903719a40e2fdef385a67ebd7ade
Bryce Palichuk <43261252+Brycepalichuk@users.noreply.github.com>
2022-05-11 21:31:23 -0600

    Add cloud function

---
+++ .history/src/cloud/CloudCategoriesFunction_20220511204459.js
@@ -0,0 +1,32 @@
+// Cloud code to watch categories event
+Moralis.Cloud.define("Content", async (request) => {
+    let options = {
+        chainId: "0x13881",
+        address: "0xFdBDf33036aA7b0DaDFBD30DDb1356468d7740a8",
+        topic: "ContentAdded(bytes32, string)",
+        abi: {
+            "anonymous": false,
+            "inputs": [
+                {
+                    "indexed": true,
+                    "internalType": "bytes32",
+                    "name": "contentId",
+                    "type": "bytes32"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "string",
+                    "name": "contentUri",
+                    "type": "string"
+                }
+            ],
+            "name": "ContentAdded",
+            "type": "event"
+        },
+        limit: 500000,
+        tableName: "ContentAdded",
+        sync_historical: false,
+    };
+    
+    Moralis.Cloud.run("watchContractEvent", options, { useMasterKey: "TlsUlqBgvYE69fHNbIXKIcLJWlCEX9oBYri2nmNF" });
+});
